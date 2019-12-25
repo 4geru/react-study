ffd07b659d818d704139f50cbc1d95d553ed04d4
Rp7rf <westhouse51@gmail.com>
2019-12-25 13:11:58 +0900

    add promise test

--- 05-jest-tutorial/src/async.test.js
+++ 05-jest-tutorial/src/async.test.js
@@ -43,3 +43,26 @@ describe('Promise', () => {
   });
 });
 
+describe('Promise', () => {
+  const fetchData = async (isSuccess) => {
+    if (!isSuccess) {
+      throw 'error!';
+    }
+    return 'peanut butter';
+  }
+
+  test('the data is peanut butter', async () => {
+    const data = await fetchData(true);
+    expect(data).toBe('peanut butter');
+  });
+
+  test('the fetch fails with an error', async () => {
+    expect.assertions(1);
+    try {
+      await fetchData(false);
+    } catch (e) {
+      expect(e).toMatch('error!');
+    }
+  });
+});
+
