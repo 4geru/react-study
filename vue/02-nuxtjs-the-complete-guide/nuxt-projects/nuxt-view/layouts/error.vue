53c387e24b4e9b6da8f6b8d40e2fc19c55e6c2c3
4geru <uchinishi.koichi@moneyforward.co.jp>
2020-04-19 17:03:03 +0900

    finished section5

---
+++ vue/02-nuxtjs-the-complete-guide/nuxt-projects/nuxt-view/layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>error</h1>
+    <p>{{ error.statusCode }}</p>
+    <p :v-if="error.statusCode == 404">404 not found</p>
+  </div>
+</template>
+
+<script>
+export default {  
+  props: ['error']
+}
+</script>
