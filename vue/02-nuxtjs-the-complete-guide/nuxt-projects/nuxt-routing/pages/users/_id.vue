0a24343fed9d4d3e6a3328aa0b94cb6cfd0aedc7
4geru <uchinishi.koichi@moneyforward.co.jp>
2020-04-19 16:39:00 +0900

    finished section4

---
+++ vue/02-nuxtjs-the-complete-guide/nuxt-projects/nuxt-routing/pages/users/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <h1>{{ message }}</h1>
+    <h3>id: {{ this.$route.params.id }}</h3>
+    <p>{{ paramsId }}</p>
+    <p>{{ p.id }}</p>
+  </section>
+</template>
+
+<script>
+export default { 
+  data: function () {
+    return {
+      message: "/users/_id.vueを表示中",
+      paramsId: this.$route.params.id
+    }
+  }, 
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+  asyncData ({ params }) {
+    return { p: params } // reference: https://qiita.com/usk81/items/9d98a337b6ef86b90404
+  }
+}
+</script>
