bd8659ee4f886383f1724a5d4d89d2fbfe490a16
4geru <westhouse51@gmail.com>
2021-07-15 09:00:32 +0900

    solved kyopuro 90 day1

---
+++ daily_kyopuro_solved_90/day1.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+
+var N, K, L int
+var A = make([]int, 1 << 18)
+
+func solve(M int) bool {
+	cnt := 0
+	pre := 0
+
+	for i := 1; i <= N; i ++ {
+		if (A[i] - pre >= M && L - A[i] >= M) {
+			cnt += 1
+			pre = A[i]
+		}
+	}
+	if(cnt >= K) {
+		return true
+	}
+	return false
+}
+
+func main(){
+	fmt.Scan(&N, &L, &K)
+	for i := 1; i <= N; i ++ {
+		fmt.Scan(&A[i])
+	}
+
+	var left = -1
+	var right = L + 1
+
+	for(right - left > 1) {
+		var mid = left + (right - left) / 2
+		if(solve(mid) == false) {
+			right = mid
+		} else {
+			left = mid
+		}
+	}
+
+	fmt.Println(left)
+	return
+}
